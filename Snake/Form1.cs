ccbdfe1566c5be0898ede7526dec6e33c59c1378
ivoPashov <ivopashov@abv.bg>
2015-09-11 17:09:31 +0300

    Initial commit

---
+++ Snake/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        public Snake Snake { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Snake = new Snake(3, 50, 50, 3);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    Snake.Move(Directions.Up);
+                    break;
+                case Keys.Down:
+                    Snake.Move(Directions.Down);
+                    break;
+                case Keys.Left:
+                    Snake.Move(Directions.Left);
+                    break;
+                case Keys.Right:
+                    Snake.Move(Directions.Right);
+                    break;
+            }
+
+            this.Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var item in Snake.SnakeBody)
+            {
+                var g = e.Graphics;
+                g.DrawRectangle(new Pen(Color.Black), item.X, item.Y, Snake.BodyPartSize, Snake.BodyPartSize);
+            }
+        }
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Snake
+{
+    public class Snake
+    {
+        public List<BodyPart> SnakeBody { get; set; }
+        public int BodyPartSize { get; set; }
+        public Directions CurrentDirection { get; set; }
+
+        public Snake(int size, int startX, int startY, int bodyPartSize)
+        {
+            this.SnakeBody = new List<BodyPart>();
+            this.BodyPartSize = bodyPartSize;
+            CurrentDirection = Directions.Right;
+
+            for (int i = 0; i < size; i++)
+            {
+                SnakeBody.Add(new BodyPart() { X = (startX - bodyPartSize * i), Y = startY });
+            }
+        }
+
+        public void Move(Directions direction)
+        {
+            if (IsDirectionAllowed(direction))
+            {
+                switch (direction)
+                {
+                    case Directions.Up:
+                        if (!DetectSelfCollision(0, -1))
+                        {
+                            Move(0, -1);
+                            this.CurrentDirection = direction;
+                        }
+                        break;
+                    case Directions.Down:
+                        if (!DetectSelfCollision(0, 1))
+                        {
+                            Move(0, 1);
+                            this.CurrentDirection = direction;
+                        }
+                        break;
+                    case Directions.Right:
+                        if (!DetectSelfCollision(1,0))
+                        {
+                            Move(1, 0);
+                            this.CurrentDirection = direction;
+                        }
+                        break;
+                    case Directions.Left:
+                        if (!DetectSelfCollision(-1, 0))
+                        {
+                            Move(-1, 0);
+                            this.CurrentDirection = direction;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Move(int xDiff, int yDiff)
+        {
+            for (int i = SnakeBody.Count - 1; i >= 1; i--)
+            {
+                SnakeBody[i].X = SnakeBody[i - 1].X;
+                SnakeBody[i].Y = SnakeBody[i - 1].Y;
+            }
+
+            SnakeBody[0].X += this.BodyPartSize * xDiff;
+            SnakeBody[0].Y += this.BodyPartSize * yDiff;
+        }
+
+        private bool IsDirectionAllowed(Directions direction)
+        {
+            if (((direction == Directions.Up && CurrentDirection == Directions.Down) || (direction == Directions.Down && CurrentDirection == Directions.Up))
+                || ((direction == Directions.Right && CurrentDirection == Directions.Left) || (direction == Directions.Left && CurrentDirection == Directions.Right))
+                )
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool DetectSelfCollision(int xDiff, int yDiff)
+        {
+            for (int i = 1; i < SnakeBody.Count; i++)
+            {
+                if(((SnakeBody[0].X + this.BodyPartSize * xDiff)==SnakeBody[i].X && (SnakeBody[0].Y + this.BodyPartSize * yDiff)==SnakeBody[i].Y)){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class BodyPart
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
